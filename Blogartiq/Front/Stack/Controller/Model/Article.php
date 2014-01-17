ffc2017c81b345578b7113eb5e74871d034726f5
Thomas ZILLIOX <thomas@zilliox.me>
2014-01-17 17:59:51 +0100

    The article must be in lowercase

--- Blogartiq/Front/Stack/Controller/Model/Article.php
+++ Blogartiq/Front/Stack/Controller/Model/Article.php
@@ -6,9 +6,13 @@ class Article extends Article\__Parent
 {
 
     public function actionView($id) {
-        $article = (new \Stack\Entity\Article)->fetchById($id);
+        $query = strtolower($id);
+        $article = (new \Stack\Entity\Article)->fetchById($query);
         if (! $article->exists()) {
             return null;
+        }
+        if ($id != $article->id) {
+            \Staq\Util::httpRedirect(\Staq\Util::getModelControllerUrl($article), 301);
         }
 
         $view = $this->createView();
